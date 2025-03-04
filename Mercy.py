1dbc756ba5e496b615acc0c165b1e43a29c86b28
1 <kirillmihal762@gmail.com>
2025-03-04 15:50:05 +0200

    Domashka

--- Mercy.py
+++ Mercy.py
@@ -7,18 +7,13 @@ def divider(a, b):
         if b > 100:
             raise IndexError("Друге число більше 100")
         return a / b
-    except TypeError as e:
-        print(f"Помилка типу: {e}")
     except ValueError as e:
          print(f"Помилка значення: {e}")
     except IndexError as e:
         print(f"Помилка індексу: {e}")
-    except ZeroDivisionError as e:
-        print(f"Ділення на нуль: {e}")
     return None
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, (1, 2, 3): 15, 8: 4}
-
+data = {10: 2, 2: 5, 25: 4, 18: 5, 15: 15, 8: 4}
 for key in data:
     res = divider(key, data[key])
     if res is not None:
