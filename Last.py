0d8a478ec35e791bee769ba79dd9af1c0423112a
1 <kirillmihal762@gmail.com>
2025-03-06 15:18:36 +0200

    Crypto

--- Last.py
+++ Last.py
@@ -1,8 +1,9 @@
 import requests
+responce = requests.get("https://bank.gov.ua/ua/markets/")
+
 from bs4 import BeautifulSoup
 
-usd = requests.get("https://bank.gov.ua/ua/markets/exchangerates")
-soup = BeautifulSoup(usd.text, "html.parser")
-
+ton = requests.get("https://bank.gov.ua/ua/markets/exchangerates")
+soup = BeautifulSoup(ton.text, features="html.parser")
 usdt = soup.find("td", string="USD").find_next("td", {"data-label": "Офіційний курс"}).text
 print(f"Курс USD: {usdt}")
